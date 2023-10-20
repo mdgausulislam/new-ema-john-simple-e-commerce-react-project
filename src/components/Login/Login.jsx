f6522926affc949a5356b89db3a396e9ea10c946
unknown <gausulislam5@gmail.com>
2023-10-20 12:17:27 +0600

    error handle and password matching

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleSignUp=event=>{
+        event.preventDefault();
+        const SignUp=event.target;
+        const email=SignUp.email.value;
+        const password=SignUp.password.value;
+        const confirmPassword=SignUp.confirm.value;
+        console.log(email,password,confirmPassword);
+
+    }
     return (
         <div className='form-container'>
             <h2 className='form-title'>Login</h2>
-            <form>
+            <form onSubmit={handleSignUp}>
                 <div className="form-control">
                     <label htmlFor="">Email</label>
                     <input type="email" name="email" id="" required />
@@ -16,6 +26,7 @@ const Login = () => {
                 </div>
                     <input className='btn-submit' type="submit" value="Login" />
             </form>
+            <p><small>New to Ema-john? <Link to='/signUp'>Create New Account</Link></small></p>
         </div>
     );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SIgnUp.css'
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+
+    const [error,setError]=useState('');
+
+    const handleSignUp=event=>{
+        event.preventDefault();
+        const formSignUp=event.target;
+        const email=formSignUp.email.value;
+        const password=formSignUp.password.value;
+        const confirmPassword=formSignUp.confirm.value;
+        console.log(email,password,confirmPassword);
+
+        if(password !==confirmPassword){
+            setError('Your password did not match');
+            return;
+        }
+        else if(password.length < 6){
+            setError('Your password must be 6 character or longer');
+            return;
+        }
+
+    }
     return (
         <div className='form-container'>
             <h2 className='form-title'>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSignUp}>
                 <div className="form-control">
                     <label htmlFor="">Email</label>
                     <input type="email" name="email" id="" required />
@@ -20,6 +42,8 @@ const SignUp = () => {
                 </div>
                 <input className='btn-submit' type="submit" value="Sign Up" />
             </form>
+            <p><small>Already Have an Account? <Link to='/login'>LogIn</Link></small></p>
+            <p className='text-error'>{error}</p>
         </div>
     );
 };
