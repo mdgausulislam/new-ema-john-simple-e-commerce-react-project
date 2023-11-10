e3661fb7723d4fc42f5cb39c1e26e3aeb2c936a0
unknown <gausulislam5@gmail.com>
2023-11-10 11:01:43 +0600

    product review error solve

--- src/loaders/cardProductsLoader.js
+++ src/loaders/cardProductsLoader.js
@@ -1,18 +1,29 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
-const cartProductsLoader=async()=>{
-    const loadedProducts=await fetch('http://localhost:5000/products');
-    const products=await loadedProducts.json();
-
+const cartProductsLoader = async () => {
     //if cart is an database, you have to use async await
-    const storedCart=getShoppingCart();
-    const saveCart=[];
+    const storedCart = getShoppingCart();
+    const ids=Object.keys(storedCart);
+    console.log(ids);
+    const loadedProducts = await fetch(`http://localhost:5000/productsById`,{
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        }, 
+        body:JSON.stringify(ids)
+    });
+    const products = await loadedProducts.json();
+    console.log('products by id :',products);
+
+
+
+    const saveCart = [];
 
     for (const id in storedCart) {
-        const addedProduct=products.find(pd=>pd._id===id);
-        if(addedProduct){
-            const quantity=storedCart[id];
-            addedProduct.quantity=quantity;
+        const addedProduct = products.find(pd => pd._id === id);
+        if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
             saveCart.push(addedProduct);
         }
     }
